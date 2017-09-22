85663f0a6dabb6efa0eddbb8a378f459e6ad5134
MaxPoole <maxmcglinnpoole@gmail.com>
2017-09-21 22:19:24 -0400

    Created TensorHelper class for tensor related operations not specific to the decomposition

---
+++ TensorHelper.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+
+class TensorHelper():
+
+    def create_matricization(self, tensor):
+        K = len(tensor)
+        I = len(tensor[0])
+        J = len(tensor[0][0])
+        X_1 = np.reshape(tensor, [I, J*K])
+        X_2 = np.reshape(tensor, [J, I*K])
+        X_3 = np.reshape(tensor, [K, I*J])
+        return X_1, X_2, X_3
