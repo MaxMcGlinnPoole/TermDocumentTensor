b3ae20238867d096650047d847e9171b8d3852c8
Donnell Muse <qq45691@umbc.edu>
2017-10-23 21:51:42 -0400

    redesgin of heatmap using heatmap class from plotly

--- TensorVisualization.py
+++ TensorVisualization.py
@@ -16,27 +16,6 @@ class TensorVisualization():
         :param data:
         :param axis_labels:
         :return:
-
-
-
-        # This makes the labels appear correctly, I do not know why
-        axis_labels_abbreviated = [""]
-        axis_labels_abbreviated.extend([label[:5] for label in axis_labels])
-        fig, ax1 = plt.subplots(1, 1)
-        labels = ax1.get_xticks().tolist()
-        plt.xticks(labels, axis_labels_abbreviated, rotation='vertical')
-        plt.yticks(labels, axis_labels_abbreviated)
-
-        ax1.yaxis.set_major_locator(MaxNLocator(len(axis_labels)))
-        ax1.xaxis.set_major_locator(MaxNLocator(len(axis_labels)))
-        ax1.imshow(data, cmap='hot')
-
-        #legend and labels
-        heatmap = ax1.pcolor(data, cmap='hot')
-        plt.title('cosine similarity between documents')
-        cbar = plt.colorbar(heatmap)
-        cbar.ax.get_yaxis().labelpad = 20
-        cbar.ax.set_ylabel('strength of relationship', rotation=270)
         """
         axis_labels_abbreviated = [""]
         axis_labels_abbreviated.extend([label[:11] for label in axis_labels])
@@ -45,7 +24,7 @@ class TensorVisualization():
                            y=axis_labels_abbreviated,
                            colorscale = 'Viridis',
                            )]
-        
+
         layout = go.Layout(title='cosine similairty between documents',
                            xaxis = dict(ticks=' '),
                            yaxis = dict(ticks=' '),
