c3248031222e50454a7da18f20e4ba3f9df427bc
Donnell Muse <qq45691@umbc.edu>
2017-10-23 21:50:00 -0400

    redesgin of heatmap using heatmap class from plotly

--- TensorVisualization.py
+++ TensorVisualization.py
@@ -1,5 +1,6 @@
 import plotly
-from plotly.graph_objs import *
+import plotly.plotly as py
+import plotly.graph_objs as go
 import matplotlib.pyplot as plt
 from matplotlib.ticker import MaxNLocator
 
@@ -15,7 +16,9 @@ class TensorVisualization():
         :param data:
         :param axis_labels:
         :return:
-        """
+
+
+
         # This makes the labels appear correctly, I do not know why
         axis_labels_abbreviated = [""]
         axis_labels_abbreviated.extend([label[:5] for label in axis_labels])
@@ -28,11 +31,29 @@ class TensorVisualization():
         ax1.xaxis.set_major_locator(MaxNLocator(len(axis_labels)))
         ax1.imshow(data, cmap='hot')
 
+        #legend and labels
         heatmap = ax1.pcolor(data, cmap='hot')
         plt.title('cosine similarity between documents')
         cbar = plt.colorbar(heatmap)
         cbar.ax.get_yaxis().labelpad = 20
         cbar.ax.set_ylabel('strength of relationship', rotation=270)
+        """
+        axis_labels_abbreviated = [""]
+        axis_labels_abbreviated.extend([label[:11] for label in axis_labels])
+        info = [go.Heatmap(z=data,
+                           x=axis_labels_abbreviated,
+                           y=axis_labels_abbreviated,
+                           colorscale = 'Viridis',
+                           )]
+        
+        layout = go.Layout(title='cosine similairty between documents',
+                           xaxis = dict(ticks=' '),
+                           yaxis = dict(ticks=' '),
+                           plot_bgcolor= '#444',
+                           paper_bgcolor= '#eee'
+                           )
+        fig = go.Figure(data=info, layout=layout)
+        plotly.offline.plot(fig,filename='malware_heatmap.html')
 
     def show(self):
         plt.show()
