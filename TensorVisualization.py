a62d05fb5c72542c4eb7cc49e112a492c0a89aa0
phanitejakesha <phani1@umbc.edu>
2018-03-07 18:40:58 -0500

    created a branch for checking accuracy

--- TensorVisualization.py
+++ TensorVisualization.py
@@ -5,7 +5,7 @@ from sklearn.decomposition import TruncatedSVD
 import time
 import scipy.cluster.hierarchy
 import scipy.spatial.distance
-
+from collections import Counter
 
 
 def flag_function_visualization(cmts):
@@ -25,28 +25,56 @@ class TensorVisualization():
         :param axis_labels:
         :return:
         """
-        if flag==1:
+        if flag == 1:
             print("Generating Heatmap ")
         axis_labels_abbreviated = [label[:14] for label in axis_labels]
         info = [go.Heatmap(z=data,
                            x=axis_labels_abbreviated,
                            y=axis_labels_abbreviated,
-                           colorscale = 'Hot',
+                           colorscale='Hot',
                            )]
 
         layout = go.Layout(title='Cosine Similarity Between Documents',
-                           xaxis = dict(ticks=' '),
-                           yaxis = dict(ticks=' '),
-                           plot_bgcolor= '#444',
-                           paper_bgcolor= '#eee'
+                           xaxis=dict(ticks=' '),
+                           yaxis=dict(ticks=' '),
+                           plot_bgcolor='#444',
+                           paper_bgcolor='#eee'
                            )
         fig = go.Figure(data=info, layout=layout)
-        plotly.offline.plot(fig,filename='malware_heatmap.html')
+        plotly.offline.plot(fig, filename='malware_heatmap.html')
 
     def k_means_clustering(self, factor_matrix, file_names=[], clusters=2):
         svd = TruncatedSVD(n_components=clusters, n_iter=7, random_state=42)
         reduced = svd.fit_transform(factor_matrix)
         kmeans = KMeans(n_clusters=clusters, random_state=0).fit(factor_matrix)
+        labels_predicted = kmeans.labels_
+        res = Counter(kmeans.labels_)
+        val = list(res.values())
+        president_data = 0
+        shakespeare_data = 0
+        president_data_correct = 0
+        shakespeare_data_correct = 0
+
+        for i in range(0, len(file_names)):
+            filename = file_names[i]
+            str = filename[1:4]
+            if TensorVisualization.RepresentsInt(str):
+                president_data = president_data + 1
+                if labels_predicted[i] == 0:
+                    president_data_correct = president_data_correct + 1
+            else:
+                shakespeare_data = shakespeare_data + 1
+                if labels_predicted[i] == 1:
+                    shakespeare_data_correct = shakespeare_data_correct + 1
+
+        print("Number of president text files are", president_data)
+        print("Number of president text files predicted correctly is", president_data)
+        print("Accuracy : ", (president_data_correct / president_data) * 100)
+
+        print("Number of shakespeare text files are", shakespeare_data)
+        print("Number of shakespeare text files predicted correctly is", shakespeare_data_correct)
+        print("Accuracy : ", (shakespeare_data_correct / shakespeare_data) * 100)
+
         data = [plotly.graph_objs.Scatter(x=[entry[0] for entry in reduced],
                                           y=[entry[1] for entry in reduced],
                                           mode='markers',
@@ -56,3 +84,11 @@ class TensorVisualization():
                 ]
         fig = go.Figure(data=data)
         plotly.offline.plot(fig, filename='kmeans_cluster.html')
+
+    def RepresentsInt(s):
+        try:
+            int(s)
+            return True
+        except ValueError:
+            return False
+
