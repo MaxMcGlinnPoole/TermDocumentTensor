c3b0a649e52cff2cb721fd02a718b81144885dfc
MaxPoole <maxmcglinnpoole@gmail.com>
2018-03-07 21:05:26 -0500

    Using additional features

--- PEFILE_Scripts/create_pe_file.py
+++ PEFILE_Scripts/create_pe_file.py
@@ -5,13 +5,16 @@ import time
 class PEFile():
     def __init__(self, file_location, file):
         self.file = file
-        self.file_header_characteristics = ["IMAGE_FILE_BYTES_REVERSED_HI", "IMAGE_FILE_DLL",
+        self.file_header_characteristics = ["IMAGE_FILE_BYTES_REVERSED_HI", "IMAGE_FILE_BYTES_REVERSED_LO",
+                                            "IMAGE_FILE_RELOCS_STRIPPED"]
+        self.sections_flags = ["IMAGE_SCN_CNT_UNINITIALIZED_DATA", "IMAGE_SCN_MEM_SHARED"]
+        """self.file_header_characteristics = ["IMAGE_FILE_BYTES_REVERSED_HI", "IMAGE_FILE_DLL",
                                             "IMAGE_FILE_DEBUG_STRIPPED", "IMAGE_FILE_LARGE_ADDRESS_AWARE",
                                             "IMAGE_FILE_LOCAL_SYMS_STRIPPED","IMAGE_FILE_RELOCS_STRIPPED",
                                             "IMAGE_FILE_LINE_NUMS_STRIPPED"]
         self.sections_flags = ["IMAGE_SCN_CNT_UNINITIALIZED_DATA", "IMAGE_SCN_ALIGN_1BYTES",
                                       "IMAGE_SCN_ALIGN_2BYTES", "IMAGE_SCN_ALIGN_4BYTES", "IMAGE_SCN_MEM_DISCARDABLE",
-                                      "IMAGE_SCN_MEM_NOT_PAGED", "IMAGE_SCN_MEM_WRITE", "IMAGE_SCN_MEM_SHARED"]
+                                      "IMAGE_SCN_MEM_NOT_PAGED", "IMAGE_SCN_MEM_WRITE", "IMAGE_SCN_MEM_SHARED"]"""
         self.file_information = []
 
         self.load_from_file(file_location)
@@ -51,18 +54,27 @@ class PEFile():
             for flag in self.sections_flags:
                 if section[flag]:
                     sections_flags.append(flag)
+            if section["SizeOfRawData"] == 0:
+                sections_flags.append("RawData_zero")
+            elif section["Misc_VirtualSize"] / section["SizeOfRawData"] > 10:
+                sections_flags.append("abnormal_virtualsize")
+            if section["PointerToLinenumbers"] != 0:
+                sections_flags.append("pointer_to_line_number")
         if len(sections_flags) == 0:
             sections_flags.append("NONE")
+
+        # File Header
         for flag in self.file_header_characteristics:
             if file_header[flag]:
                 characteristics.append(flag)
-        if file_header['NumberOfSections'] > 8 or file_header['NumberOfSections'] < 1:
+        if file_header['NumberOfSections'] > 9 or file_header['NumberOfSections'] < 1:
             various.append("sections_abnormal")
         else:
             various.append("sections_normal")
         if file_header['PointerToSymbolTable'] > 0:
             various.append("debugging_info")
 
+        # IMPORTS
         if hasattr(pe, "DIRECTORY_ENTRY_IMPORT"):
             for entry in pe.DIRECTORY_ENTRY_IMPORT:
                 dlls.append(entry.dll.decode("utf-8"))
@@ -75,12 +87,21 @@ class PEFile():
         else:
             functions.append("NONE")
             dlls.append("NONE")
+
+        #Optional Header
         size = os.stat(file_location).st_size
         code_ratio = opt_header["SizeOfCode"]/size
         initial_ratio = opt_header["SizeOfInitializedData"]/size
         uninitial_ratio = opt_header["SizeOfUninitializedData"]/size
         image_ratio = opt_header["SizeOfImage"]/size
         header_ratio = opt_header["SizeOfHeaders"]/size
+        address_ratio = opt_header["AddressOfEntryPoint"]/size
+        base_code_ratio = opt_header["BaseOfCode"]/size
+        try:
+            base_data_ratio = opt_header["BaseOfData"]/size
+        except:
+            base_data_ratio = 0
+
         if code_ratio > 1:
             sizes.append("code_abnormal")
         if initial_ratio > 3:
@@ -91,6 +112,16 @@ class PEFile():
             sizes.append("image_abnormal")
         if header_ratio > 0:
             sizes.append("header_abnormal")
+        if address_ratio > 2:
+            sizes.append("address_abnormal")
+        if base_code_ratio > 2:
+            sizes.append("base_code_abnormal")
+        if base_data_ratio > 4:
+            sizes.append("base_data_abnormal")
+            print(file_location)
+        if opt_header["NumberOfRvaAndSizes"] != 16:
+            various.append("Rva_abnormal")
+
 
 
         self.file_information.append(functions)
